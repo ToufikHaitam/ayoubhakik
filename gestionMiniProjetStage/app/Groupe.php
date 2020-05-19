eccf1ba939deb2eb483c3ebad6d1865bbcc1b437
bootstrap <44092019+ToufikHaitam@users.noreply.github.com>
2020-05-19 17:54:26 +0000

    Update Groupe.php

--- gestionMiniProjetStage/app/Groupe.php
+++ gestionMiniProjetStage/app/Groupe.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Groupe extends Model
 {
-    //
+    // pour dire que votre model est associe a une table dans Db qui s'appel groups 
+    public $table="groups "
+    
+    
+    // list des etudiants par exemple sera comme suit 
+     public function  etudiants(){
+        
+        return $this>hasMany(Etudiant::class);
+        
+        
+    }
+    
+    // pour avoir les groups et etudaiants vous aallez juste Faire 
+     Groupe::with('etudians ')->get();
+    
+    // comme ca vous aurez la liste des groupes avec les etudiants dans chaque groupe
 }
